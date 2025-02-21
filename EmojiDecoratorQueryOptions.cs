961ab5b26a7318f2449e783bfc2aa42ca5ead144
APIVerve Team <hello@apiverve.com>
2025-02-21 07:33:28 -0600

    Added new API Endpoints

---
+++ EmojiDecoratorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class EmojiDecoratorQueryOptions {
+public string text { get; set; }
+}
+
+}
